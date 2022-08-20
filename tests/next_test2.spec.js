095bf499a53fadd0595255d5b4c8a2265520e1a2
LightEmber <b.dylykov54@gmail.com>
2022-08-21 00:48:13 +0800

    add previous_test2, update next_test2

--- tests/next_test2.spec.js
+++ tests/next_test2.spec.js
@@ -1,12 +1,12 @@
 const { test, expect } = require('@playwright/test');
 
-test('button next', async ({ page }) => {
-  // Go to https://demoqa.com/books
-  await page.goto('https://demoqa.com/books');
-  // Select 5
-  await page.locator('[aria-label="rows per page"]').selectOption('5');
-  // Click text=Next
-  await page.locator('text=Next').click();
-  let page2 = page.locator('text=Page of 2')
-  await expect(page2).toBeVisible();
+test('check button next', async ({ page }) => {
+   // Go to https://demoqa.com/books
+   await page.goto('https://demoqa.com/books');
+   // Select 5
+   await page.locator('[aria-label="rows per page"]').selectOption('5');
+   await page.locator('.-next').click();
+   let jmptpg = await page.locator('[aria-label="jump to page"]')
+   let page_number = await jmptpg.getAttribute("value")
+   await expect(page_number).toEqual("2")
 });
